50c06cc9ee3ba7c0a9c7ac0cd14ce854aa3b093f
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2020-08-06 21:17:27 +0100

    format document

--- src/js/script.js
+++ src/js/script.js
@@ -1,66 +1,64 @@
-const databaseName = document.getElementById('dbName');
-const databaseVersion = document.getElementById('dbVersion');
-const btn = document.getElementById('createDB');
-const insert = document.getElementById('insert');
-const view = document.getElementById('view');
+const databaseName = document.getElementById("dbName");
+const databaseVersion = document.getElementById("dbVersion");
+const btn = document.getElementById("createDB");
+const insert = document.getElementById("insert");
+const view = document.getElementById("view");
 let database = null;
 
 btn.onclick = createDataBase;
 insert.onclick = insertPost;
 view.onclick = viewPost;
 
-
-
 function viewPost() {
-    let tx = database.transaction('personal_posts', 'readonly');
-    let post = tx.objectStore('personal_posts');
-    let request = post.openCursor()
-    request.onsuccess = (e) => {
-        let cursor = e.target.result;
-        if (cursor) {
-             cursor.continue()
-             }
+  let tx = database.transaction("personal_posts", "readonly");
+  let post = tx.objectStore("personal_posts");
+  let request = post.openCursor();
+  request.onsuccess = (e) => {
+    let cursor = e.target.result;
+    if (cursor) {
+      cursor.continue();
     }
-    
+  };
 }
 
-
 function insertPost() {
-    post =  {
-        title: "post1",
-        text: "my first post"
-    }
+  post = {
+    title: "post1",
+    text: "my first post",
+  };
 
-    let tx = database.transaction('personal_posts', 'readwrite');
-    console.log(tx) // Error : DOMException: Key already exists in the object store
-    tx.onerror = (err) => {
-        alert(err.target.error);
-    }
-    let myPost = tx.objectStore('personal_posts')
-    myPost.add(post);
+  let tx = database.transaction("personal_posts", "readwrite");
+  console.log(tx); // Error : DOMException: Key already exists in the object store
+  tx.onerror = (err) => {
+    alert(err.target.error);
+  };
+  let myPost = tx.objectStore("personal_posts");
+  myPost.add(post);
 }
 
 function createDataBase() {
-    if(databaseVersion.value.length == 0) databaseVersion.value = 1;
-const request = indexedDB.open(`${databaseName.value}`, databaseVersion.value);
+  if (databaseVersion.value.length == 0) databaseVersion.value = 1;
+  const request = indexedDB.open(
+    `${databaseName.value}`,
+    databaseVersion.value
+  );
 
+  request.onupgradeneeded = function (e) {
+    database = e.target.result;
 
-
-
-
-request.onupgradeneeded = function(e) {
-    database = e.target.result
-
-
-    const personalPosts = database.createObjectStore("personal_posts", {keyPath: "title"})
-    const favouriteMovies = database.createObjectStore('favourite_movies', {keyPath: 'action'});
+    const personalPosts = database.createObjectStore("personal_posts", {
+      keyPath: "title",
+    });
+    const favouriteMovies = database.createObjectStore("favourite_movies", {
+      keyPath: "action",
+    });
     alert(`Upgrade database to ${database.name} version ${database.version}`);
-}
-request.onsuccess = function(e) {
+  };
+  request.onsuccess = function (e) {
     database = e.target.result;
     alert(`Success on ${database.name} ${database.version}`);
-}
-request.onerror = function(err) {
-    alert('Oups, make sure you select the right version.');
-}
+  };
+  request.onerror = function (err) {
+    alert("Oups, make sure you select the right version.");
+  };
 }
